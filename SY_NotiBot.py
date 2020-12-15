b08d086f6bbdc9f9f053991169b7f2c862997f6a
seoyeonK <hasunnyha@gmail.com>
2020-12-15 23:44:58 +0900

    add telepot module

--- SY_NotiBot.py
+++ SY_NotiBot.py
@@ -1,12 +1,37 @@
 import requests
 import os
-
+import json
+import telepot
+from telepot.loop import MessageLoop 
 
 token = os.getenv('TELEGRAM_TOKEN')
+bot = telepot.Bot(token)
 
-# 업데이트 내용 받아오기
-# 아래의 주소를 호출하면, 업데이트 된 봇의 내용을 가져올 수 있다.
+def get_latest_msg():
+   # msg = json.loads(requests.get(receive_url).text)["result"][-1]["message"]["text"] # json으로 받기
+    msg = bot.getUpdates()[-1]["message"]["text"]
+    return msg
+ 
+print(get_latest_msg())
 
-url = 'https://api.telegram.org/bot{}/getUpdates'.format(token)
-response = json.loads(requests.get(url).text) # json으로 받기
-print(response)
+
+chat_id = bot.getUpdates()[-1]["message"]["from"]["id"]
+def handle(msg):
+
+    if get_latest_msg() == 'search':
+       #if the user wants to search a specific watch w/ serial number
+        txt = "input the serial number of the watch"
+        bot.sendMessage(chat_id, txt)
+        return
+       #ask for date
+
+    SN = int(get_latest_msg())
+    if SN < 250 :
+        bot.sendMessage("good job")
+
+    #print list of the uploaded videos
+    #can i get the videos too? can i play it at telegram?
+
+#connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+#defining handle fuction -> need to set precise algorithm for coding
